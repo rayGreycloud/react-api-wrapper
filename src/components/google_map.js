e312be20bd77920dc36cf3a54e215ef6b5d5d2bb
rayGreycloud <rayGreycloud@gmail.com>
2017-05-07 06:21:06 -0700

    Add lifecycle methods to custom lib component to control rendering

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
+// Custom libary to be implemented
 export default class extends Component {
+  // Only render component one time
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  componentDidMount() {
+    // Create component
+    this.map = new google.maps.Map(this.refs.map , {
+      center: { lat: this.props.lat, lng: this.props.lng },
+      zoom: 8
+    });
+  }
   render() {
     return (
-      <div id="map">
-
-      </div>
+      // Ref hooks element to component
+      <div id="map" ref="map"></div>
     );
   }
 }
