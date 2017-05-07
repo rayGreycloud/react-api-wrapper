51912206ecb79a1569a5a56a278b3b8700c06c8d
rayGreycloud <rayGreycloud@gmail.com>
2017-05-07 07:25:14 -0700

    Add componentWillReceiveProps lifecycle method to pass new props to custom lib (googlemaps) despite component update set to false

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -8,8 +8,27 @@ export default class extends Component {
     return false;
   }
 
+  // But pass new props received to custom lib for rendering
+  componentWillReceiveProps(nextProps) {
+    let pos = {
+      lat: nextProps.lat,
+      lng: nextProps.lng
+    };
+    // Move to new position
+    this.map.panTo(pos);
+    // Set new marker
+    this.marker = new google.maps.Marker({
+      position: pos,
+      map: this.map
+    });
+  }
+
+  // Render custom component
   componentDidMount() {
-    let pos = {lat: this.props.lat, lng: this.props.lng};
+    let pos = {
+      lat: this.props.lat,
+      lng: this.props.lng
+    };
 
     // Create component and append to element
     this.map = new google.maps.Map(this.refs.map , {
