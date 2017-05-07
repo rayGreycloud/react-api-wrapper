64ad5987944eba39343a4a5d166e703f43e8ca7e
rayGreycloud <rayGreycloud@gmail.com>
2017-05-07 07:22:51 -0700

    Add button to change map location (state)

--- src/components/app.js
+++ src/components/app.js
@@ -4,13 +4,20 @@ import GoogleMap from './google_map';
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { lat: -34.397, lng: 150.644 };
   }
 
   render() {
     return (
-      <div style={{height: '100%'}}>
-        Map Me!
-        <GoogleMap lat={45.562020} lng={-122.636817}/>
+      <div className="wrapper">
+        <div>
+          <h3>Map Me!</h3>
+          <button onClick={() => this.setState({ lat: 45.562020, lng: -122.636817 })} className="btn btn-info">
+            Home
+          </button>
+        </div>
+        <GoogleMap lat={this.state.lat} lng={this.state.lng}/>
       </div>
     );
   }
